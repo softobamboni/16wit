72bfcc8a48ef720465e4987da1f8bd644ff44064
Softobamboni <119755471+softobamboni@users.noreply.github.com>
2025-09-10 18:36:36 +0200

    Move calc_origin to another file

--- rectangle.asm
+++ rectangle.asm
@@ -62,39 +62,6 @@ fill_area:					;arguments: x ($34), y ($36), width ($30), height ($32), color (c
 @end:
  	bra @end
 
-calc_origin:				;calculate origin
-	rep #$20				;needed for every draw subroutine internally
-	.a16
-	lda ypoint
-	asl
-	asl
-	asl
-	asl
-	asl
-	asl
-	sta temp
-	lda ypoint
-	asl
-	asl
-	asl
-	asl
-	clc
-	adc temp
-	sta ycalcd
-	lda xpoint
-	and #$0007
-	tax
-	lda xpoint
-	lsr
-	lsr
-	lsr
-	sta xcalcd
-	stx nibble
-	lda xcalcd
-	clc
-	adc ycalcd		;0+x+y = x+y
-	sta origin
-	rts
 width_draw:			;subroutine (args = origin (memory), width (memory), carry (1 = change color))
 	.a16
 	.i8
